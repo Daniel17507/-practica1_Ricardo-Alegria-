91510a1db1d42f55d97fb80ea7ee24ed850c910b
Daniel17507 <89824101+Daniel17507@users.noreply.github.com>
2021-09-06 17:45:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+function getIMC(){
+  var peso = parseFloat(documento. getElementById('peso').valor);
+  var altura = parseFloat(documento. getElementById('altura').valor);
+  var imc= peso / (altura *altura);
+  documento.getElementById("imc"). innerHTML=imc;
+}
